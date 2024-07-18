dfd4d154a6fa935fc44acb30d9b33f24063e96e3
vinay <vinay.shermauryan@gmail.com>
2024-07-18 21:01:15 +0530

    redux-persist setup

--- Daily class code/20240628 react-mui flipkart-clone/index.js
+++ Daily class code/20240628 react-mui flipkart-clone/index.js
@@ -6,16 +6,20 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./src/redux/store";
+import { persistor, store } from "./src/redux/store";
+import { PersistGate } from "redux-persist/es/integration/react";
+import BackdropLoader from "./src/components/BackdropLoader";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <PersistGate loading={<BackdropLoader />} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </>
 );
--- Daily class code/20240628 react-mui flipkart-clone/src/redux/store.js
+++ Daily class code/20240628 react-mui flipkart-clone/src/redux/store.js
@@ -2,11 +2,23 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { setupListeners } from "@reduxjs/toolkit/query";
 import appReducer from "./appReducer/appReducer";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, appReducer);
 
 export const store = configureStore({
   reducer: {
-    app: appReducer,
+    app: persistedReducer,
   },
 });
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
